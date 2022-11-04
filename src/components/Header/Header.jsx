dac2af96f6aed1b1d944e1a152ec49899394bd55
obordonado <obordonado@protonmail.com>
2022-11-04 12:39:30 +0100

    fix:changed logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import './Header.scss'
 const Header = (props) => {
   return(
     <header>
-      <img className='logo' src="https://img.freepik.com/vector-gratis/ilustracion-icono-dibujos-animados-cono-helado-concepto-icono-comida-dulce-aislado-estilo-dibujos-animados-plana_138676-2924.jpg?w=826&t=st=1667561736~exp=1667562336~hmac=708538e54237dd546bf7266d5f858dee6e916584327045eab7b3404fbe59962e"/>
+      <img className='logo' src="https://i.pinimg.com/originals/52/45/95/5245959f20a2d092cdde319bfe1b2d74.jpg"/>
       <NavLink to= "/">
       <span className="inicio">Home</span>
       </NavLink>
